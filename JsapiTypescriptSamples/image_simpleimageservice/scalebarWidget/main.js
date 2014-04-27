fceb63955d6d9f6c580dae6fee8d5709b5b0ca1c
Amr <eldib.amr@gmail.com>
2014-04-26 17:57:28 -0700

    Sample IDs and Links to JSAPI Samples
    - Added links in startup page to JSAPI samples.
    - Added extra property in samples.json for sample IDs. This ID will be
    used to refer to JSAPI samples and folders containing TypeScript
    samples.
    - Added folder for image_simpleimageservice

---
+++ JsapiTypescriptSamples/image_simpleimageservice/scalebarWidget/main.js
@@ -0,0 +1,5 @@
+﻿define(["require", "exports", "./ScalebarController"], function(require, exports, ScalebarController) {
+    var scalebarController = new ScalebarController("mapDiv", "topo");
+    scalebarController.start();
+});
+//# sourceMappingURL=main.js.map
---
+++ JsapiTypescriptSamples/image_simpleimageservice/scalebarWidget/main.ts
@@ -0,0 +1,4 @@
+﻿import ScalebarController = require("./ScalebarController");
+
+var scalebarController = new ScalebarController("mapDiv", "topo");
+scalebarController.start();
