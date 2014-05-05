8676ce4e5e8c1b730c83c7b44697a39efdf3799c
Adam <amarinelli@esri.ca>
2014-05-05 17:03:49 -0400

    Minor fixes and formatting for Fade Effect sample
    - moved named functions to outside start() function
    - moved class variables outside of function
    - declared map as Map object type
    - still experiencing BorderContainer error...will issue a Bug

--- ags_fade_webmaps/fadeWebmaps/FadeWebmapsController.js
+++ ags_fade_webmaps/fadeWebmaps/FadeWebmapsController.js
@@ -10,90 +10,90 @@ define(["require", "exports", "dojo/on", "dojo/dom", "dijit/registry", "dojo/dom
         }
         FadeWebmapsController.prototype.start = function () {
             try  {
-                var currentMap, previousMap, resizeHandle, counter = -1, webmaps = [
+                this.counter = -1;
+                this.webmaps = [
                     "d94dcdbe78e141c2b2d3a91d5ca8b9c9",
                     "d802f08316e84c6592ef681c50178f17",
                     "dbbe6ca611ba460ea9a3ba5133d54969",
                     "f28762ef94ef4700864fd57d0ef7ec7a"
                 ];
 
-                on(dom.byId("show_next"), "click", nextMap);
-                loadNext();
-
-                function loadNext(map) {
-                    //create the content pane for the map
-                    var mapPane = new ContentPane({
-                        "content": "",
-                        "id": "map" + (++counter),
-                        "region": "center"
-                    }, domConstruct.create("div"));
-
-                    //add the newly constructed content pane to the page
-                    registry.byId(this.mainWindow).addChild(mapPane);
-
-                    var deferred;
-                    if (map && map.hasOwnProperty("extent")) {
-                        deferred = arcgisUtils.createMap((webmaps[counter % webmaps.length]), mapPane.domNode.id, {
-                            mapOptions: {
-                                extent: map.extent
-                            }
-                        });
-                    } else {
-                        deferred = arcgisUtils.createMap((webmaps[counter % webmaps.length]), mapPane.domNode.id);
-                    }
-                    deferred.then(function (response) {
-                        dom.byId("current_map").innerHTML = response.itemInfo.item.title;
-                        fadeMap(response.map);
-                    });
-                }
-
-                function fadeMap(map) {
-                    currentMap = map;
-
-                    if (currentMap.loaded) {
-                        if (previousMap) {
-                            // References:
-                            // http://dojotoolkit.org/documentation/tutorials/1.6/effects/
-                            // http://dojotoolkit.org/documentation/tutorials/1.6/animation/
-                            var combinedAnim = coreFx.combine([
-                                baseFx.fadeIn({
-                                    node: currentMap.container,
-                                    duration: 1000
-                                }),
-                                baseFx.fadeOut({
-                                    node: previousMap.container,
-                                    duration: 1000,
-                                    onEnd: removePrevious
-                                })
-                            ]);
-                            combinedAnim.play();
-                        } else {
-                            baseFx.fadeIn({ node: currentMap.container }).play();
-                        }
-                    } else {
-                        // handle map onLoad from webmap
-                        map.on("load", function () {
-                            fadeMap(map);
-                        });
-                    }
-                }
-
-                function removePrevious() {
-                    previousMap.destroy();
-                    domConstruct.destroy(previousMap.container);
-                }
-
-                function nextMap() {
-                    if (currentMap) {
-                        previousMap = currentMap;
-                        loadNext(currentMap);
-                    }
-                    ;
-                }
-                ;
+                on(dom.byId("show_next"), "click", this.nextMap);
+                this.loadNext();
             } catch (error) {
                 console.log("ERROR " + error);
             }
+        };
+
+        FadeWebmapsController.prototype.loadNext = function (map) {
+            //create the content pane for the map
+            var mapPane = new ContentPane({
+                "content": "",
+                "id": "map" + (++this.counter),
+                "region": "center"
+            }, domConstruct.create("div"));
+
+            //add the newly constructed content pane to the page
+            registry.byId(this.mainWindow).addChild(mapPane);
+
+            var deferred;
+            if (map && map.hasOwnProperty("extent")) {
+                deferred = arcgisUtils.createMap((this.webmaps[this.counter % this.webmaps.length]), mapPane.domNode.id, {
+                    mapOptions: {
+                        extent: map.extent
+                    }
+                });
+            } else {
+                deferred = arcgisUtils.createMap((this.webmaps[this.counter % this.webmaps.length]), mapPane.domNode.id);
+            }
+            deferred.then(function (response) {
+                dom.byId("current_map").innerHTML = response.itemInfo.item.title;
+                this.fadeMap(response.map);
+            });
+        };
+
+        FadeWebmapsController.prototype.fadeMap = function (map) {
+            this.currentMap = map;
+
+            if (this.currentMap.loaded) {
+                if (this.previousMap) {
+                    // References:
+                    // http://dojotoolkit.org/documentation/tutorials/1.6/effects/
+                    // http://dojotoolkit.org/documentation/tutorials/1.6/animation/
+                    var combinedAnim = coreFx.combine([
+                        baseFx.fadeIn({
+                            node: this.currentMap.container,
+                            duration: 1000
+                        }),
+                        baseFx.fadeOut({
+                            node: this.previousMap.container,
+                            duration: 1000,
+                            onEnd: this.removePrevious
+                        })
+                    ]);
+                    combinedAnim.play();
+                } else {
+                    baseFx.fadeIn({ node: this.currentMap.container }).play();
+                }
+            } else {
+                // handle map onLoad from webmap
+                map.on("load", function () {
+                    this.fadeMap(map);
+                });
+            }
+        };
+
+        FadeWebmapsController.prototype.removePrevious = function () {
+            this.previousMap.destroy();
+            domConstruct.destroy(this.previousMap.container);
+        };
+
+        FadeWebmapsController.prototype.nextMap = function () {
+            if (this.currentMap) {
+                this.previousMap = this.currentMap;
+                this.loadNext(this.currentMap);
+            }
+            ;
         };
         return FadeWebmapsController;
     })();
